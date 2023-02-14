fd8bee479821f191ca99acf688f24e5fd53f7ab5
michelmyseth <38660333+michelmyseth@users.noreply.github.com>
2023-02-14 11:32:08 +0100

    maj pajento

--- cypress/e2e/majento.cy.js
+++ cypress/e2e/majento.cy.js
@@ -17,7 +17,7 @@ beforeEach(() => {
 });
 
 describe("parcours utilisateur shopping", () => {
-    it.skip("insciption", () => {
+    it("insciption", () => {
         cy.get(".panel > .header > :nth-child(3) > a").click();
         cy.get("#firstname").type(name);
         cy.get("#lastname").type(last);
@@ -28,7 +28,7 @@ describe("parcours utilisateur shopping", () => {
             "#form-validate > .actions-toolbar > div.primary > .action > span"
         ).click();
     });
-    it.skip("connexion", () => {
+    it("connexion", () => {
         cy.get(".panel > .header > .authorization-link > a").click();
         cy.get("#email").type("d.vador@yopmail.com");
         cy.get(
@@ -39,7 +39,7 @@ describe("parcours utilisateur shopping", () => {
         ).click();
     });
 
-    it.skip("ajouter panier", () => {
+    it("ajouter panier", () => {
         cy.get("#ui-id-4").trigger("mouseover");
         cy.get("#ui-id-9").trigger("mouseover");
         cy.get("#ui-id-11 > span").click();
@@ -57,7 +57,7 @@ describe("parcours utilisateur shopping", () => {
         cy.get(".showcart").click();
         cy.wait(3000);
         cy.get("#top-cart-btn-checkout").click();
-        cy.wait(2000);
+        cy.wait(3000);
         cy.get("#customer-email-fieldset > .required").type(email);
         cy.get('[name="shippingAddress.firstname"]').type(name);
         cy.get('[name="shippingAddress.lastname"]').type(last);
@@ -99,11 +99,11 @@ describe("parcours utilisateur shopping", () => {
 
         cy.get('input[class="input-text qty"]').clear().type("20");
         cy.get(".update > span").click();
-        cy.wait(1000);
+        cy.wait(3000);
         cy.get(
             ".checkout-methods-items > :nth-child(1) > .action > span"
         ).click();
-        cy.wait(1000);
+        cy.wait(6000);
         cy.get("#customer-email-fieldset > .required").type(email);
         cy.get('[name="shippingAddress.firstname"]').type(name);
         cy.get('[name="shippingAddress.lastname"]').type(last);
@@ -117,9 +117,11 @@ describe("parcours utilisateur shopping", () => {
         cy.get(":nth-child(2) > :nth-child(1) > .radio").click();
         cy.get(".button").click();
         cy.get("#billing-address-same-as-shipping-checkmo").click();
+        cy.wait(3000);
         cy.get(
             ".payment-method-content > :nth-child(4) > div.primary > .action"
         ).click();
+        cy.wait(3000);
     });
     it("mise a jour adresse livraison", () => {
         cy.get("#ui-id-4").trigger("mouseover");
@@ -135,12 +137,12 @@ describe("parcours utilisateur shopping", () => {
         cy.get('[class="action tocart primary"]')
             .first()
             .click({ force: true });
-        cy.wait(2000);
+        cy.wait(4000);
         cy.get(".showcart").click();
-        cy.wait(2000);
+        cy.wait(4000);
         cy.get(":nth-child(7) > .secondary > .action > span").click();
         cy.get(".update > span").click();
-        cy.wait(2000);
+        cy.wait(4000);
         cy.get(
             ".checkout-methods-items > :nth-child(1) > .action > span"
         ).click();
@@ -150,7 +152,6 @@ describe("parcours utilisateur shopping", () => {
         cy.get('[name="shippingAddress.company"]').type(compagny);
         cy.get('[name="shippingAddress.street.0"]').type(address);
         cy.get('[name="shippingAddress.city"]').type(city);
-        // cy.get('[class="select"]').eq(0).select("Alaska");
         cy.get('[name="shippingAddress.region_id"]')
             .find('select[class="select"]')
             .select("Alaska");
@@ -159,13 +160,12 @@ describe("parcours utilisateur shopping", () => {
         cy.get('[name="shippingAddress.country_id"]')
             .find('select[class="select"]')
             .select("France");
-        // cy.get('[class="select"]').eq(1).select("France");
         cy.get('[name="shippingAddress.telephone"]').type(phone);
         cy.get(":nth-child(2) > :nth-child(1) > .radio").click();
         cy.get(".button").click();
         cy.get("#billing-address-same-as-shipping-checkmo").click();
         cy.get("#billing-address-same-as-shipping-checkmo").click();
-        cy.wait(1000);
+        cy.wait(3000);
         cy.get('[name="billingAddresscheckmo.firstname"]').type(name);
         cy.get('[name="billingAddresscheckmo.lastname"]').type(last);
         cy.get('[name="billingAddresscheckmo.company"]').type(compagny);
